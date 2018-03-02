51561d285f2344add73e45b645929370b2137eed
xiaoyustudio181 <35361157+xiaoyustudio181@users.noreply.github.com>
2018-03-02 14:49:07 +0800

    20180302.1449

--- helper.js
+++ helper.js
@@ -84,3 +84,36 @@ Array.prototype.remove = function(element) {//数组移除指定元素
     if (index > -1)this.splice(index, 1);
 };
 //=================================================
+function Request(method,func,data) {
+    this.TestMode=0;
+    this.ip='localhost';
+    this.baseUrl='http://'+this.ip+'/Project/index.php/Admin/Ajax/';
+    return this.request(method,func,data);
+}
+Request.prototype.request = function (method,func,data) {
+    var TestMode=this.TestMode;
+    var url=this.baseUrl+func+'.html';
+    var type=method?'POST':'GET';
+    if(TestMode){
+        console.warn(type+' 向 '+url+' 发送数据：');
+        console.log(data);
+    }
+    var ajax=$.ajax({
+        url:url,
+        type:type,    //发送请求的方式
+        //async:true,//异步，只能在回调函数中获取值；
+        async:false,//同步，此时外面才能获取到回调函数中的值。
+        data:data,
+        dataType:'json',    //期待的返回数据的类型（注意字母大小写）
+        success:function(data){    //接收成功，参数是返回的数据
+            if(TestMode){
+                console.warn(type+' 从 '+url+' 收到数据：');
+                console.log(data);
+            }
+        },
+        error:function(xhr,status,message){
+            console.log('异常消息: '+status+' '+message);
+        }
+    });
+    return ajax.responseJSON;
+};
